137ce6b506ee4e34b2ff8ee71d88367887d90e66
Samar Yadav <hello.samar7@gmail.com>
2025-12-03 21:18:52 +0530

    fixed self-connection issue

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,28 @@ io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
   socket.on('find-partner', () => {
-    if (waitingUsers.length > 0) {
-      const partner = waitingUsers.shift();
+    // First, remove this socket from waiting queue if it's already there
+    const existingIndex = waitingUsers.findIndex(user => user.id === socket.id);
+    if (existingIndex !== -1) {
+      waitingUsers.splice(existingIndex, 1);
+      console.log(`Removed ${socket.id} from waiting queue (duplicate request)`);
+    }
+
+    // Find a partner that is NOT the same socket
+    let partner = null;
+    while (waitingUsers.length > 0) {
+      const potentialPartner = waitingUsers.shift();
       
+      // Make sure we're not matching with ourselves
+      if (potentialPartner.id !== socket.id) {
+        partner = potentialPartner;
+        break;
+      } else {
+        console.log(`Skipped self-match for ${socket.id}`);
+      }
+    }
+
+    if (partner) {
       activeConnections.set(socket.id, partner.id);
       activeConnections.set(partner.id, socket.id);
 
@@ -43,6 +62,7 @@ io.on('connection', (socket) => {
       
       console.log(`Matched ${socket.id} (offerer) with ${partner.id} (answerer)`);
     } else {
+      // No valid partner found, add to waiting queue
       waitingUsers.push(socket);
       socket.emit('waiting');
       console.log(`${socket.id} added to waiting queue`);
